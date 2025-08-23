064c5a7a1fc42b70ff0dfe71d1e47575ea9f0ae4
LightShi233 <lightshi233@outlook.com>
2025-08-24 00:11:05 +0800

    arrow -ify

--- arrow_cannon.mk
+++ arrow_cannon.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
 
 # Inherit from cannon device
 $(call inherit-product, device/xiaomi/cannon/device.mk)
 
 PRODUCT_DEVICE := cannon
-PRODUCT_NAME := lineage_cannon
+PRODUCT_NAME := arrow_cannon
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2007J22C
 PRODUCT_MANUFACTURER := Xiaomi
